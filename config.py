c9090e3a3411f6cba83d7e605b25e07d1191d6ec
Omer <feedodeedo@gmail.com>
2023-03-07 11:56:13 -0500

    added os for environ vars

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 import os
 
+
 class Config(object):
     """Configuration for the app"""
-    
-    SQLALCHEMY_DATABASE_URI = (
-        os.environ.get('DATABASE_URL', 'postgresql:///stocksim_db'))
-    
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL', 'postgresql:///stocksim_db')
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
-    SECRET_KEY = "abcdefghijk"
+    SECRET_KEY = os.environ.get("SECRET_KEY", "secretkey_local")
     DEBUG_TB_INTERCEPT_REDIRECTS = False
